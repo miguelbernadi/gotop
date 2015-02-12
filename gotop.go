ea347e28cd52ecd104654ea3e894bc0f04b9dc8f
Miguel Bernabeu <miguelbernadi@gmail.com>
2015-02-12 17:24:44 +0100

    Distinguish process related structure from rest of /proc

--- gotop.go
+++ gotop.go
@@ -4,18 +4,30 @@ import(
     "os"
     "fmt"
     "log"
+    "io/ioutil"
 )
 
 func main () {
-
-    file, err := os.Open("/proc/cpuinfo") // For read access.
+    base := "/proc"
+    dirs, err := ioutil.ReadDir(base)
     if err != nil {
         log.Fatal(err)
     }
-    data := make([]byte, 100)
-    count, err := file.Read(data)
-    if err != nil {
-        log.Fatal(err)
+     var process_list []os.FileInfo
+    for _, v := range(dirs){
+        // If it is a directory and contains a file named cmdline it is a process
+        if v.IsDir() {
+            cmdline_name := base + "/" + v.Name() + "/cmdline"
+            file, err := os.Open(cmdline_name)
+            defer file.Close()
+            if err == nil {
+                process_list = append(process_list, v)
+            }
+        } else {
+            fmt.Println(v.Name())
+        }
     }
-    fmt.Printf("read %d bytes: %q\n", count, data[:count])
+    for _, v := range(process_list) {
+        fmt.Println(v.Name())
+    }
 }
